a27291bbc66f2a123e3a70d38856c7cc120416fb
holmesjj <hujiajun19961024@gmail.com>
2025-07-27 07:20:36 +0800

    Update

--- test3.py
+++ test3.py
@@ -12,7 +12,10 @@ def extract_mp3(mp4_path):
         'ffmpeg', '-y',
         '-i', mp4_path,
         '-q:a', '0',
-        '-map', 'a', mp3_path
+        '-map', '0:a:0',
+        '-vn',
+        '-acodec', 'libmp3lame',
+        mp3_path
     ]
     subprocess.run(cmd, check=True)
     return mp3_path
